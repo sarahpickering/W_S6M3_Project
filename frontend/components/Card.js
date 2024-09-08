756096779de62d47fac5d0419a4abc2bf2cd4a85
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-09-07 17:21:56 -0700

    added styling to card. Practiced creating a styled div and adding props to components.

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  border: 1px solid maroon;
+  border-radius: 12px;
+  padding: 2rem;
+  background-color: whitesmoke;
+  
+
+  h2 {
+    color: navy;
+    font-size: 3em;
+    margin: 0 0 1rem 0;
+  }
+
+  p {
+    background-color: whitesmoke;
+    margin: 0 0 1rem 0;
+
+    &::first-line {
+      font-size: 1.5em;
+    }
+  }
+  `;
 
 export default function Card({ title, text, imageURL, date }) {
     return (
-      <div className='card'>
+      <StyledCard $color="lime" className='card'>
           <h2>{title}</h2>
           <p>{text}</p>
           <Figure
             imageURL={imageURL}
             caption={date}
             />
-          </div>
+          </StyledCard>
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+  max-width: 70%;
+
+  figcaption {
+    font-style: bold, italic;
+  }
+
+`
 
 export default function Figure({ imageURL, caption }) {
     return  (
